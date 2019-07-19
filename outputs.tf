4db39a83d0e0be110cbf7ff51d9166d4de8e3d11
tim_clegg <tim.clegg@oracle.com>
2019-07-19 09:40:06 -0600

    Updating outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
 
 #########################
-## VCN
+## Subnets
 #########################
 output "subnets" {
   description = "The returned resource attributes for the subnets."
@@ -9,4 +9,12 @@ output "subnets" {
     for x in oci_core_subnet.this:
       x.display_name => x
   }
+}
+
+#########################
+## ADs
+#########################
+output "ads" {
+  description = "The different Availability Domains available."
+  value       = data.oci_identity_availability_domains.this
 }
