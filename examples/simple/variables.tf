6804a7354372e3fa95aa4db347b467b31bd07055
Jeevan Joseph <jeevan.g@gmail.com>
2019-07-24 22:01:27 -0700

    changes to credentials

--- examples/simple/variables.tf
+++ examples/simple/variables.tf
@@ -2,7 +2,7 @@
 variable "tenancy_id" {}
 variable "user_id" {}
 variable "fingerprint" {}
-variable "private_key" {}
+variable "private_key_path" {}
 variable "region" {}
 variable "default_compartment_id" {}
 
