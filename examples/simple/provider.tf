098ea4087201f555aabbb4d089803ea956173aee
Jeevan Joseph <jeevan.g@gmail.com>
2019-07-24 17:27:27 -0700

    upated provider

--- examples/simple/provider.tf
+++ examples/simple/provider.tf
@@ -2,7 +2,7 @@ provider "oci" {
   tenancy_ocid     = "${var.tenancy_id}"
   user_ocid        = "${var.user_id}"
   fingerprint      = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
+  private_key_path = "../../creds/api_key.pem"
   region           = "${var.region}"
 }
 
